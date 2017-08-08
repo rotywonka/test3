10a6a2db4c86962b466af4a4018e8fb0078344fe
Tommy Tan <tommytan@3c-15-c2-ce-6a-9e.wireless.temple.edu>
2017-08-08 13:28:29 -0400

    second commit

--- test3/ViewController.swift
+++ test3/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("fu shakthi")
+        print("fu josh no macbook")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
