3a4ab5c0ceeb2de54627214c7e0b5faaa6b12360
Tommy Tan <tommytan@3c-15-c2-ce-6a-9e.wireless.temple.edu>
2017-08-08 13:21:43 -0400

    Initial Commit

---
+++ test3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test3
+//
+//  Created by Tommy Tan on 8/8/17.
+//  Copyright © 2017 Tommy Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
